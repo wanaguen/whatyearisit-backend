a4e926a0c3ccfb8f31cccde11ee83f88d73a01d3
Your name <Your email>
2025-04-11 11:27:49 +0200

    whatyearisit-backend

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/year', (req, res) => {
+  const year = new Date();
+  res.json({ now: year.getFullYear() });
+ });
+
+module.exports = router;
